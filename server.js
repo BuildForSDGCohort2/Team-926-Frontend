fa3948e7b163a31896f7ded458c2fed0fc701e31
Justine Robert Igune <igunejustinerobert@gmail.com>
2020-09-05 20:31:25 +0300

    Save changes

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const {v4: uuidv4} = require("uuid");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
   /*res.status(200).send("Hello African Health Store");*/
   res.redirect(`/${uuidv4()}`); /*Using string literals*/
-})
+});
 
 app.get('/:room', (req, res) => {
   res.render('room', { roomId: req.params.room })
