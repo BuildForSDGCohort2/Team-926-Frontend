6ba1c615f347472a16c8c352f00763a34fa60869
Justine Robert Igune <igunejustinerobert@gmail.com>
2020-09-05 17:34:08 +0300

    Modified files

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const {v4: uuidv4} = require("uuid");
-
-
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 app.get("/", (req, res)=>{
   /*res.status(200).send("Hello African Health Store");*/
-  res.render("room");
+  res.redirect(`/${uuidv4()}`); /*Using string literals*/
+})
+
+app.get('/:room', (req, res) => {
+  res.render('room', { roomId: req.params.room })
 })
 
 
