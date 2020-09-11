d3c2436ba07a0ea938415daec118114b6eb4686c
Justine Robert Igune <igunejustinerobert@gmail.com>
2020-09-11 21:11:47 +0300

    Modified files

--- public/script.js
+++ public/script.js
@@ -64,38 +64,14 @@ socket.on("createMessage", message => {
   $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
   scrollToBottom()
 })
-})
 
 socket.on('user-disconnected', userId => {
 if (peers[userId]) peers[userId].close()
 })
 
-myPeer.on('open', id => {
+peer.on('open', id => {
 socket.emit('join-room', ROOM_ID, id)
 })
-
-function connectToNewUser(userId, stream) {
-const call = myPeer.call(userId, stream)
-const video = document.createElement('video')
-call.on('stream', userVideoStream => {
-  addVideoStream(video, userVideoStream)
-})
-call.on('close', () => {
-  video.remove()
-})
-
-peers[userId] = call
-}
-
-function addVideoStream(video, stream) {
-video.srcObject = stream
-video.addEventListener('loadedmetadata', () => {
-  video.play()
-})
-videoGrid.append(video)
-}
-
-
 
 const scrollToBottom = () => {
 var d = $('.main__chat_window');
