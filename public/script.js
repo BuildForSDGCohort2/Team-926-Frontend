d13cb7ad4829cf0391f45e6876e2d0e87454ecdd
Justine Robert Igune <igunejustinerobert@gmail.com>
2020-09-12 14:36:11 +0300

    Sorted out files

--- public/script.js
+++ public/script.js
@@ -34,7 +34,7 @@ navigator.mediaDevices.getUserMedia({
   // when press enter send message
   $("html").keydown(function (e) {
     console.log(text.val());
-    if (e.which == 13 && text.val().length !== 0) {
+    if (e.which === 13 && text.val().length !== 0) {
       socket.emit("message", text.val());
       text.val("")
     }
